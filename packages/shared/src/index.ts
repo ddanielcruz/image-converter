e28dade101546ccaf239c311c6b734c3db08078c
Daniel Cruz <danielcunha54@gmail.com>
2022-07-22 22:50:12 -0300

    chore: export created errors

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,6 @@
+export * from './errors/bad-request-error'
+export * from './errors/custom-error'
+export * from './errors/internal-server-error'
+export * from './errors/not-found-error'
+export * from './errors/unauthorized-error'
+export * from './errors/validation-error'
